e442ac0dc39b57478416ed2afefad0cfaa26ed3e
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2022-09-28 00:45:11 -0300

    adiciona material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Button, Typography, Box, Container, Link } from '@mui/material'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -6,28 +7,32 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+    <Container className="App">
+      <Box>
+        <Link href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
+        </Link>
+        <Link href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        </Link>
+      </Box>
+      <Typography variant='h2'>Vite + React</Typography>
+      <Box className='card'>
+        <Button
+          onClick={() => setCount((count) => count + 1)}
+          variant='contained'
+          color='info'
+        >
           count is {count}
-        </button>
-        <p>
+        </Button>
+        <Typography mt={2}>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+        </Typography>
+      </Box>
+      <Typography className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      </Typography>
+    </Container>
   )
 }
 
