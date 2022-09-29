a5a4998bd09f6557b68cbd1943bbc06331b342c5
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2022-09-28 22:44:22 -0300

    adiciona eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Button, Typography, Box, Container, Link } from '@mui/material'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
