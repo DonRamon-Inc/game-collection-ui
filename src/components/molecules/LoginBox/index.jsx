080ea996b40e01b2e4869f9fbc6ac678fd23b089
RamonRCM <costa.m.ramon@gmail.com>
2023-02-03 19:10:40 -0300

    validacao nao-exibindo

--- src/components/molecules/LoginBox/index.jsx
+++ src/components/molecules/LoginBox/index.jsx
@@ -2,7 +2,30 @@ import { Button } from '../../atoms/Button'
 import { TextField } from '../../atoms/TextField'
 import { Grid } from '@mui/material'
 
+import * as Yup from 'yup'
+import { Formik, Form } from 'formik'
 export function LoginBox () {
+  const data = ({
+    email: '',
+    senha: ''
+  })
+
+  const validacaoForm = Yup.object().shape({
+    email: Yup.string()
+      .required('Campo obrigatório')
+      .matches(
+        /^\w+([.!#$%&'*/=?^_+\-`{|}~]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        'Email invalido, favor verificar email'
+      ),
+    senha: Yup.string().required('Campo obrigatório').matches(/^.{8,100}/,
+      'Senha inválida. Sua senha deve conter entre 8 a 100 caracteres'
+    )
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&+\-_*/=\\\s'`~´:?<>.,;{}()[\]])[A-Za-z\d!@#$%&+\-_*/=\\\s'`~´:?<>.,;{}()[\]]{8,100}$/,
+        'Senha inválida. A senha precisa conter uma combinação de ' +
+        'letras (maiscula e minuscula), números e símbolos')
+  })
+
   return (
     <Grid
       container
@@ -11,15 +34,22 @@ export function LoginBox () {
       alignItems="center"
       spacing={1}
       >
-        <Grid item>
-          <TextField label='Email' type='email'/>
-        </Grid>
-        <Grid item>
-          <TextField label='Senha' type='password'/>
-        </Grid>
-        <Grid item>
-          <Button text='Login'></Button>
-        </Grid>
+        <Formik
+          initialValues={data}
+          validationSchema={validacaoForm}
+          >
+            <Form>
+              <Grid item>
+                <TextField label='Email' type='email'/>
+              </Grid>
+              <Grid item>
+                <TextField label='Senha' type='password'/>
+              </Grid>
+              <Grid item>
+                <Button text='Login'></Button>
+              </Grid>
+          </Form>
+        </Formik>
       </Grid>
 
   )
